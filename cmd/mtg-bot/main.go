2531def5a87b65afc2a4f4238eb2dd00f2edb080
Karma-234 <busari1kamal@gmail.com>
2026-03-07 23:48:00 +0100

    Add: graceful shutdown

--- cmd/mtg-bot/main.go
+++ cmd/mtg-bot/main.go
@@ -3,6 +3,11 @@ package main
 import (
 	"log"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"gopkg.in/telebot.v4"
 )
 
 func main() {
@@ -11,4 +16,36 @@ func main() {
 		log.Fatal("TG_BOT_API_KEY is not set")
 	}
 
+	pref := telebot.Settings{
+		Token:  apiKey,
+		Poller: &telebot.LongPoller{Timeout: 10 * time.Second},
+	}
+	b, _ := telebot.NewBot(pref)
+	me := b.Me
+	log.Printf("Bot username: %s", me.Username)
+	cmdsn := []telebot.Command{
+		{Text: "start", Description: "Starts the bot"},
+	}
+	err := b.SetCommands(cmdsn)
+	if err != nil {
+		log.Printf("Error setting commands: %v", err)
+	}
+
+	b.Handle("/start", func(ctx telebot.Context) error {
+		sender := ctx.Sender()
+		chat_id := ctx.Chat().ID
+		log.Printf("Received /start command from user %s (ID: %d) in chat %d", sender.Username, sender.ID, chat_id)
+		return ctx.Send("Hello, " + sender.Username + "!")
+	})
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		log.Printf("Bot started with username: %s", me.Username)
+		b.Start()
+	}()
+	<-done
+	b.Stop()
+	log.Println("Bot stopped")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/karma-234/mtg-bot
 
 go 1.25.0
+
+require gopkg.in/telebot.v4 v4.0.0-beta.7 // indirect
