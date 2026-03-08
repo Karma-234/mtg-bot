d0ba7f9781c08a0cf40b6a3eb7b8183cdf03d661
Karma-234 <busari1kamal@gmail.com>
2026-03-08 04:34:13 +0100

    Add: multi button

--- cmd/mtg-bot/main.go
+++ cmd/mtg-bot/main.go
@@ -30,23 +30,42 @@ func main() {
 	if err != nil {
 		log.Printf("Error setting commands: %v", err)
 	}
-	startButton := &telebot.ReplyMarkup{}
-	fetcherBtn := startButton.Data("Fetch latest MTG news", "fetch-news")
-	startButton.Inline(startButton.Row(fetcherBtn))
+	startButtonMarkup := &telebot.ReplyMarkup{}
+	fetcherBtn := startButtonMarkup.Data("Fetch latest MTG news", "fetch-news")
+	startButtonMarkup.Inline(startButtonMarkup.Row(fetcherBtn))
+
+	taskDurationMarkup := &telebot.ReplyMarkup{}
+	setTask1hDurationBtn := taskDurationMarkup.Data("1 hour", "dur_1h")
+	setTask3hDurationBtn := taskDurationMarkup.Data("3 hours", "dur_3h")
+	setTask6hDurationBtn := taskDurationMarkup.Data("6 hours", "dur_6h")
+	taskDurationMarkup.Inline(taskDurationMarkup.Row(setTask1hDurationBtn, setTask3hDurationBtn, setTask6hDurationBtn))
 
 	b.Handle("/start", func(ctx telebot.Context) error {
 		sender := ctx.Sender()
 
 		log.Printf("Received /start command from user %s (ID: %d) in chat %d", sender.Username, sender.ID, sender.ID)
-		return ctx.Send("Hello, "+sender.Username+"!\n Here are the available services:\n\n", startButton)
+		return ctx.Send("Hello, "+sender.Username+"!\n Here are the available services:\n\n", startButtonMarkup)
 	})
 	b.Handle(&fetcherBtn, func(ctx telebot.Context) error {
 		log.Printf("Received fetch news request from user %s (ID: %d) in chat %d", ctx.Sender().Username, ctx.Sender().ID, ctx.Chat().ID)
 
 		ctx.Edit("Fetching latest MTG news...", &telebot.SendOptions{ReplyMarkup: &telebot.ReplyMarkup{}})
 		time.Sleep(3 * time.Second)
-		return ctx.Send("Here is the  latest MTG news...", &telebot.SendOptions{ReplyMarkup: startButton})
+		return ctx.Send("Here is the  latest MTG news...", &telebot.SendOptions{ReplyMarkup: taskDurationMarkup})
 	})
+
+	durationHandler := func(duration time.Duration) telebot.HandlerFunc {
+		return func(ctx telebot.Context) error {
+			log.Printf("Received task duration selection '%s' from user %s (ID: %d) in chat %d", duration.String(), ctx.Sender().Username, ctx.Sender().ID, ctx.Chat().ID)
+
+			ctx.Edit("You selected task duration: "+duration.String(), &telebot.SendOptions{ReplyMarkup: &telebot.ReplyMarkup{}})
+
+			return ctx.Respond(&telebot.CallbackResponse{Text: "Task duration set to " + duration.String()})
+		}
+	}
+	b.Handle(&setTask1hDurationBtn, durationHandler(1*time.Minute))
+	b.Handle(&setTask3hDurationBtn, durationHandler(3*time.Minute))
+	b.Handle(&setTask6hDurationBtn, durationHandler(6*time.Minute))
 
 	done := make(chan os.Signal, 1)
 	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
