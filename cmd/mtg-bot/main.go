a499638a709418d3ccfd1710b2b3e56ddc4cdd96
Karma-234 <busari1kamal@gmail.com>
2026-04-06 18:42:33 +0100

    refactor: extract telegram handlers and markup from main

--- cmd/mtg-bot/main.go
+++ cmd/mtg-bot/main.go
@@ -6,8 +6,8 @@ import (
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
 
+	"github.com/karma-234/mtg-bot/internal/bothandlers"
 	"github.com/karma-234/mtg-bot/internal/botruntime"
 	"gopkg.in/telebot.v4"
 )
@@ -40,50 +40,7 @@ func main() {
 	if err != nil {
 		log.Printf("Error setting commands: %v", err)
 	}
-	startButtonMarkup := &telebot.ReplyMarkup{}
-	fetcherBtn := startButtonMarkup.Data("Bybit Agent", "bybit_fetcher")
-	startButtonMarkup.Inline(startButtonMarkup.Row(fetcherBtn))
-
-	taskDurationMarkup := &telebot.ReplyMarkup{}
-	setTask1hDurationBtn := taskDurationMarkup.Data("1 hour", "dur_1h")
-	setTask3hDurationBtn := taskDurationMarkup.Data("3 hours", "dur_3h")
-	setTask6hDurationBtn := taskDurationMarkup.Data("6 hours", "dur_6h")
-	taskDurationMarkup.Inline(taskDurationMarkup.Row(setTask1hDurationBtn, setTask3hDurationBtn, setTask6hDurationBtn))
-
-	b.Handle("/start", func(ctx telebot.Context) error {
-		sender := ctx.Sender()
-
-		log.Printf("Received /start command from user %s  ", sender.Username)
-		return ctx.Send("Hello, "+sender.Username+"!\n Here are the available services:\n\n", startButtonMarkup)
-	})
-	b.Handle(&fetcherBtn, func(ctx telebot.Context) error {
-		log.Printf("Received Bybit Agent request from user %s ", ctx.Sender().Username)
-
-		if err := ctx.Edit("Fetching latest MTG news...", &telebot.SendOptions{ReplyMarkup: &telebot.ReplyMarkup{}}); err != nil {
-			log.Printf("Failed to edit message for user %s: %v", ctx.Sender().Username, err)
-		}
-		return ctx.Send("Here is the  latest MTG news...", &telebot.SendOptions{ReplyMarkup: taskDurationMarkup})
-	})
-
-	durationHandler := func(duration time.Duration) telebot.HandlerFunc {
-		return func(ctx telebot.Context) error {
-			log.Printf("Received task duration selection '%s' from user %s", duration.String(), ctx.Sender().Username)
-
-			if err := ctx.Edit("You selected Bybit Agent for duration: "+duration.String(), &telebot.SendOptions{ReplyMarkup: &telebot.ReplyMarkup{}}); err != nil {
-				log.Printf("Failed to edit duration selection for user %s: %v", ctx.Sender().Username, err)
-			}
-			taskManager.Schedule(b, duration, ctx.Chat(), merchantService)
-
-			if err := ctx.Respond(&telebot.CallbackResponse{Text: "Task duration set to " + duration.String()}); err != nil {
-				log.Printf("Failed to send callback response to user %s: %v", ctx.Sender().Username, err)
-				return err
-			}
-			return nil
-		}
-	}
-	b.Handle(&setTask1hDurationBtn, durationHandler(1*time.Hour))
-	b.Handle(&setTask3hDurationBtn, durationHandler(3*time.Hour))
-	b.Handle(&setTask6hDurationBtn, durationHandler(6*time.Hour))
+	bothandlers.RegisterHandlers(b, taskManager, merchantService)
 
 	done := make(chan os.Signal, 1)
 	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
---
+++ internal/bothandlers/handlers.go
@@ -0,0 +1,72 @@
+package bothandlers
+
+import (
+	"log"
+	"time"
+
+	"github.com/karma-234/mtg-bot/internal/botruntime"
+	"github.com/karma-234/mtg-bot/internal/service"
+	"gopkg.in/telebot.v4"
+)
+
+type DurationButtons struct {
+	OneHour    telebot.Btn
+	ThreeHours telebot.Btn
+	SixHours   telebot.Btn
+}
+
+func BuildStartMarkup() (*telebot.ReplyMarkup, telebot.Btn) {
+	markup := &telebot.ReplyMarkup{}
+	fetcherBtn := markup.Data("Bybit Agent", "bybit_fetcher")
+	markup.Inline(markup.Row(fetcherBtn))
+	return markup, fetcherBtn
+}
+
+func BuildDurationMarkup() (*telebot.ReplyMarkup, DurationButtons) {
+	markup := &telebot.ReplyMarkup{}
+	buttons := DurationButtons{
+		OneHour:    markup.Data("1 hour", "dur_1h"),
+		ThreeHours: markup.Data("3 hours", "dur_3h"),
+		SixHours:   markup.Data("6 hours", "dur_6h"),
+	}
+	markup.Inline(markup.Row(buttons.OneHour, buttons.ThreeHours, buttons.SixHours))
+	return markup, buttons
+}
+
+func RegisterHandlers(b *telebot.Bot, taskManager *botruntime.TaskManager, merchantService *service.MerchantService) {
+	startMarkup, fetcherBtn := BuildStartMarkup()
+	durationMarkup, durationButtons := BuildDurationMarkup()
+
+	b.Handle("/start", func(ctx telebot.Context) error {
+		sender := ctx.Sender()
+		log.Printf("Received /start command from user %s  ", sender.Username)
+		return ctx.Send("Hello, "+sender.Username+"!\n Here are the available services:\n\n", startMarkup)
+	})
+
+	b.Handle(&fetcherBtn, func(ctx telebot.Context) error {
+		log.Printf("Received Bybit Agent request from user %s ", ctx.Sender().Username)
+		if err := ctx.Edit("Fetching latest MTG news...", &telebot.SendOptions{ReplyMarkup: &telebot.ReplyMarkup{}}); err != nil {
+			log.Printf("Failed to edit message for user %s: %v", ctx.Sender().Username, err)
+		}
+		return ctx.Send("Here is the  latest MTG news...", &telebot.SendOptions{ReplyMarkup: durationMarkup})
+	})
+
+	durationHandler := func(duration time.Duration) telebot.HandlerFunc {
+		return func(ctx telebot.Context) error {
+			log.Printf("Received task duration selection '%s' from user %s", duration.String(), ctx.Sender().Username)
+			if err := ctx.Edit("You selected Bybit Agent for duration: "+duration.String(), &telebot.SendOptions{ReplyMarkup: &telebot.ReplyMarkup{}}); err != nil {
+				log.Printf("Failed to edit duration selection for user %s: %v", ctx.Sender().Username, err)
+			}
+			taskManager.Schedule(b, duration, ctx.Chat(), merchantService)
+			if err := ctx.Respond(&telebot.CallbackResponse{Text: "Task duration set to " + duration.String()}); err != nil {
+				log.Printf("Failed to send callback response to user %s: %v", ctx.Sender().Username, err)
+				return err
+			}
+			return nil
+		}
+	}
+
+	b.Handle(&durationButtons.OneHour, durationHandler(1*time.Hour))
+	b.Handle(&durationButtons.ThreeHours, durationHandler(3*time.Hour))
+	b.Handle(&durationButtons.SixHours, durationHandler(6*time.Hour))
+}
